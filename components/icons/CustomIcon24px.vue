1dcfeb173c77aa5b116ab1cc58b53a937ab7d619
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-17 00:50:33 +0330

    Impl CustomIcon24px.vue

---
+++ components/icons/CustomIcon24px.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-img class="new-icon" :src="iconSrc" alt="" />
+</template>
+
+<script>
+export default {
+  name: 'CustomIcon24px',
+  props: { iconSrc : String}
+}
+</script>
+
+<style scoped>
+.new-icon {
+  height: 24px;
+  width: 24px;
+}
+
+</style>
