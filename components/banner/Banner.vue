6ea0b3b3b4fe9f1c92a0b7d901b238e14ce9ce24
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-17 00:51:02 +0330

    impl Banner.vue

---
+++ components/banner/Banner.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <v-carousel
+    cycle
+    height="150"
+
+    hide-delimiters
+    show-arrows
+  >
+    <v-carousel-item
+      v-for="(slide, i) in banners"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+name: "Banner",
+  data () {
+    return {
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      banners: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
