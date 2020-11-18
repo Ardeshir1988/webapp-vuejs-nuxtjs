0f285e863511a08b95fea85023896a7ae57c6a9f
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-18 19:57:19 +0330

    modified SectionSeparator.vue

--- components/separator/SectionSeparator.vue
+++ components/separator/SectionSeparator.vue
@@ -1,25 +1,25 @@
 <template>
   <v-row class="divider-container" no-gutters>
-      <v-col>
-        <v-row  justify="end" no-gutters>
+    <v-col>
+      <v-row justify="end" no-gutters>
         <v-col>
           <div class="section-link-text">مشاهده همه</div>
         </v-col>
-          <v-col>
-            <v-row  justify="end" no-gutters>
-              <v-col cols="10">
-            <div class="section-name">محصولات جدید</div>
-              </v-col>
-              <v-col cols="auto">
-                <CustomIcon24px  icon-src="/storefront-24px.svg" />
-              </v-col>
+        <v-col>
+          <v-row justify="end" no-gutters>
+            <v-col cols="10">
+              <div class="section-name">{{sectionName}}</div>
+            </v-col>
+            <v-col cols="auto">
+              <CustomIcon24px :icon-src="sectionIcon" />
+            </v-col>
 
-            </v-row>
-          </v-col>
+          </v-row>
+        </v-col>
 
-        </v-row>
+      </v-row>
 
-        <div class="divider-line" />
+      <div class="divider-line" />
     </v-col>
 
   </v-row>
@@ -27,10 +27,15 @@
 
 <script>
 import CustomIcon24px from '@/components/icons/CustomIcon24px'
+
 export default {
   name: 'SectionSeparator',
   components: { CustomIcon24px },
-  props: ['section-name', 'section-icon', 'section-link-url']
+  props: {
+    sectionName: String,
+    sectionIcon: String,
+    sectionLinkUrl: String
+  }
 }
 </script>
 
@@ -39,14 +44,17 @@ export default {
   background-color: gray;
   height: 0.4vh;
 }
-.divider-container{
+
+.divider-container {
   margin-left: 0.5vh;
   margin-right: 0.5vh;
 }
-.section-name{
+
+.section-name {
   direction: rtl;
 }
-.section-link-text{
+
+.section-link-text {
   color: #41b883;
 }
 </style>
