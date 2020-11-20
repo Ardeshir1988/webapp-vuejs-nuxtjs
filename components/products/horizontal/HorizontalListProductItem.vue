7e75c3fe3e9dea050d730ec133da536cdff5dd21
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-20 21:47:02 +0330

    impl HorizontalListProductItem.vue & HorizontalProductList.vue

---
+++ components/products/horizontal/HorizontalListProductItem.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-sheet class="product" width="115" outlined rounded>
+    <div class="measureAndImg">
+      <p class="measure">{{ product.measure }}</p>
+      <v-img class="img-product" contain height="100" :src="imageUrl" />
+    </div>
+    <div class="productName">{{ product.name }}</div>
+    <div class="productPrice">{{ this.engDigitToPersianPrice(product.price) }}</div>
+    <v-divider></v-divider>
+    <div class="cart-operation">
+      <v-img class="cart-icon" contain src="/cart_grocery_store_green-24px.svg" alt="" />
+    </div>
+  </v-sheet>
+</template>
+
+<script>
+import { FILES_URL } from '@/constants'
+import PersianUtil from '@/utils/PersianUtil'
+
+export default {
+  name: 'HorizontalProductItem',
+  props: { product: Object },
+  computed: {
+    imageUrl: function() {
+      return FILES_URL + this.product.pictures[0]
+    }
+  },
+  methods: {
+    engDigitToPersianPrice: function(val) {
+      return PersianUtil.makePersianPrice(val)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.img-product {
+  margin: 0.5vh;
+  grid-column: 1;
+  grid-row: 2;
+}
+
+.product {
+  margin: 0.5vh;
+}
+
+.measure {
+  grid-column: 1;
+  grid-row: 1;
+  direction: rtl;
+  z-index: 33;
+  color: #808080;
+  max-lines: 1;
+  overflow: hidden;
+  background-color: #EDEDED;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  max-width: 6.2vh;
+  border-top-left-radius: 2px;
+  border-bottom-right-radius: 2px;
+  height: max-content;
+  font-size: 8pt;
+}
+
+.measureAndImg {
+  display: grid;
+  grid-template-rows: 0 2fr;
+  grid-template-columns: 1fr
+}
+
+.cart-operation {
+
+}
+
+.cart-icon {
+  margin-left: auto;
+  margin-right: auto;
+  height: 24px;
+  width: 24px;
+  margin-top: 0.2vh;
+  margin-bottom: 0.2vh;
+}
+
+.productName {
+  direction: rtl;
+  color: #808080;
+  font-size: 9pt;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  margin-right: 0.5vh;
+  margin-left: 0.5vh;
+}
+
+.productPrice {
+  direction: rtl;
+  color: #808080;
+  font-size: 9pt;
+  overflow: hidden;
+  display: -webkit-box;
+  text-align: left;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  margin-right: 0.5vh;
+  margin-left: 0.5vh;
+}
+</style>
---
+++ components/products/horizontal/HorizontalProductList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="horizontal-list">
+    <div v-for="product in newProducts">
+      <HorizontalProductItem :product="product" :key="product.id" />
+    </div>
+  </div>
+</template>
+<script>
+import HorizontalProductItem from '@/components/products/horizontal/HorizontalListProductItem'
+
+export default {
+  name: 'HorizontalProductList',
+  components: { HorizontalProductItem },
+  props: { newProducts: Array }
+}
+</script>
+
+<style scoped>
+
+.horizontal-list::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.horizontal-list {
+  flex-direction: row-reverse;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  display: flex;
+  overflow-x: auto;
+}
+</style>
