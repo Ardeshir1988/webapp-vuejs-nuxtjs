f018eeeb5e4d87f4a713d25935975dea04d50953
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-20 21:45:22 +0330

    impl PersianUtil.js

---
+++ utils/PersianUtil.js
@@ -0,0 +1,20 @@
+function makePersianPrice(input) {
+  if (input === undefined)
+    return
+  const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+  return input
+    .toString()
+    .replace(/\d/g, x => farsiDigits[x])+'تومان'
+}
+
+function covertEngDigitToPersianDigit(input) {
+  if (input === undefined)
+    return
+  const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+  return input
+    .toString()
+    .replace(/\d/g, x => farsiDigits[x])
+}
+
+
+export default { makePersianPrice,covertEngDigitToPersianDigit };
