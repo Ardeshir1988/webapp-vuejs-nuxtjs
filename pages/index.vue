b46f17251ced84b3dd2d4a3dc36649814ed5e6b0
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-21 19:16:20 +0330

    impl first step of home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,70 @@
 <template>
-<v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-        <div class="text-center">
+  <div>
+    <Banner :banners="homepage.banners" />
+    <SectionSeparator class="section-separator"
+                      section-name="محصولات جدید"
+                      section-icon="storefront-24px.svg"
+                      section-link-url="" />
+    <HorizontalProductList :new-products="homepage.newProducts" />
+    <SectionSeparator class="section-separator"
+                      section-name="دسته بندی محصولات"
+                      section-icon="category-24px.svg"
+                      section-link-url="" />
+    <CategoryList :categories="homepage.categories" />
+    <Banner class="section-separator" :banners="homepage.banners" />
 
-        </div>
-<!--        <v-card>-->
-<!--            <v-card-title class="headline">-->
-<!--                Welcome to the Vuetify + Nuxt.js template xxxxxxxxxxxxxxxxxxxxxx-->
-<!--            </v-card-title>-->
-<!--            <v-card-text>-->
-<!--                <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>-->
-<!--                <p>-->
-<!--                    For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank" rel="noopener noreferrer">-->
-<!--                        documentation-->
-<!--                    </a>.-->
-<!--                </p>-->
-<!--                <p>-->
-<!--                    If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" rel="noopener noreferrer" title="chat">-->
-<!--                        discord-->
-<!--                    </a>.-->
-<!--                </p>-->
-<!--                <p>-->
-<!--                    Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" rel="noopener noreferrer" title="contribute">-->
-<!--                        issue board-->
-<!--                    </a>.-->
-<!--                </p>-->
-<!--                <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>-->
-<!--                <div class="text-xs-right">-->
-<!--                    <em><small>&mdash; John Leider</small></em>-->
-<!--                </div>-->
-<!--                <hr class="my-3">-->
-<!--                <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">-->
-<!--                    Nuxt Documentation-->
-<!--                </a>-->
-<!--                <br>-->
-<!--                <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer">-->
-<!--                    Nuxt GitHub-->
-<!--                </a>-->
-<!--            </v-card-text>-->
-<!--            <v-card-actions>-->
-<!--                <v-spacer />-->
-<!--                <v-btn color="primary" nuxt to="/inspire">-->
-<!--                    Continue-->
-<!--                </v-btn>-->
-<!--            </v-card-actions>-->
-<!--        </v-card>-->
-      <H1>Home</H1>
-    </v-col>
-</v-row>
+    <SectionSeparator class="section-separator"
+                      section-name="شرکای تجاری"
+                      section-icon="partner-24px.svg"
+                      section-link-url="" />
+
+    <BusinessPartnerList :business-partners="homepage.businessPartners" />
+    <SectionSeparator class="section-separator"
+                      section-name="حراج"
+                      section-icon="storefront-24px.svg"
+                      section-link-url="" />
+    <HorizontalProductList :new-products="homepage.offProducts" />
+  </div>
+
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Banner from '@/components/banner/Banner'
+import SectionSeparator from '@/components/separator/SectionSeparator'
+import { mapState } from 'vuex'
+import HorizontalProductItem from '@/components/products/horizontal/HorizontalListProductItem'
+import HorizontalProductList from '@/components/products/horizontal/HorizontalProductList'
+import CategoryList from '@/components/category/CategoryList'
+import BusinessPartnerList from '@/components/partner/BusinessPartnerList'
 
 export default {
-    components: {
-        Logo,
-        VuetifyLogo
-    }
+  components: {
+    BusinessPartnerList,
+    CategoryList,
+    HorizontalProductList,
+    HorizontalProductItem,
+    SectionSeparator,
+    Banner,
+    Logo,
+    VuetifyLogo
+  },
+  async fetch({ store }) {
+    await store.dispatch('product/get_home_page')
+  },
+  computed: {
+    ...mapState({
+      homepage: (state) => {
+        return state.product.homePage
+      }
+    })
+  }
 }
 </script>
-<style>
+<style scoped>
+.section-separator {
+  margin-top: 1vh;
+  margin-bottom: 1vh;
+}
 </style>
