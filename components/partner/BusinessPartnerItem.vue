bd0cad7fa7900cd8ceff384f3afcdff9a2d67aec
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-21 19:14:25 +0330

    impl BusinessPartnerItem.vue and BusinessPartnerList.vue components

---
+++ components/partner/BusinessPartnerItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-sheet width="119" outlined rounded>
+    <v-img contain class="img-businessPartner" height="110" :src="imageUrl">
+    </v-img>
+  </v-sheet>
+</template>
+
+<script>
+import { FILES_URL } from '@/constants'
+
+export default {
+  name: 'BusinessPartnerItem',
+  props: { partner: Object },
+  computed: {
+    imageUrl: function() {
+      return FILES_URL + this.partner.picture
+    }
+  }
+}
+</script>
+
+<style scoped>
+.img-businessPartner {
+  display: flex;
+  align-items: baseline;
+  text-align: center;
+}
+
+.name-businessPartner {
+  font-size: 10pt;
+  align-items: center;
+}
+</style>
---
+++ components/partner/BusinessPartnerList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="partners">
+    <div v-for="businessPartner in businessPartners" class="partners_item">
+      <BusinessPartnerItem :partner="businessPartner" :key="businessPartner.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BusinessPartnerItem from '@/components/partner/BusinessPartnerItem'
+export default {
+  name: 'BusinessPartnerList',
+  components: { BusinessPartnerItem },
+  props: { businessPartners: Array }
+}
+</script>
+
+<style scoped>
+.partners {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.partners_item {
+  margin-bottom: 2vh;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
