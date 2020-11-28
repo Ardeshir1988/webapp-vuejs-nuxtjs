13f254e52a174c40f36df2e594e61226996f3fe9
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-11-28 12:01:35 +0330

    impl first step of HorizontalTypeList.vue

---
+++ components/type/HorizontalTypeList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="horizontal-type-list">
+    <div v-for="type in typeList">
+      <v-btn  outlined depressed color="back" class="btn-type" :value="type.id">{{ type.name }}</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HorizontalTypeList',
+  props: { typeList: Array },
+  selectedType: { name: 'همه', id: 'ALL' }
+}
+</script>
+
+<style scoped>
+.horizontal-type-list::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.horizontal-type-list {
+  flex-direction: row-reverse;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  display: flex;
+  overflow-x: auto;
+}
+.btn-type{
+  margin: 0.25vh 0.5vh 0.25vh 0.5vh;
+
+}
+</style>
