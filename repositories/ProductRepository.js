50d8adfb8a08816021ad987fd55413b8037efe72
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-02 19:32:42 +0330

    add search api call

--- repositories/ProductRepository.js
+++ repositories/ProductRepository.js
@@ -12,14 +12,17 @@ export default ($axios) => ({
   getTypesByCategoryId(categoryId) {
     return $axios.get(`${resource}/category/${categoryId}/type`)
   },
-  getSimilarProductsByProductId(productId,size,from) {
+  getSimilarProductsByProductId(productId, size, from) {
     return $axios.get(`${resource}/similar/${productId}?size=${size}&from=${from}`)
   },
-  getOffProducts(size,from) {
+  getOffProducts(size, from) {
     return $axios.get(`${resource}/off-products?size=${size}&from=${from}`)
   },
-  getNewProducts(size,from) {
+  getNewProducts(size, from) {
     return $axios.get(`${resource}/new-products?size=${size}&from=${from}`)
+  },
+  searchProducts(keyword, size, from) {
+    return $axios.get(`${resource}/search?keyword=${keyword}&size=${size}&from=${from}`)
   },
   getProductById(productId) {
     return $axios.get(`${resource}/${productId}`)
