1776e45ffb11e455c0bbdcf6777ab6e8c2a08bdd
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-02 16:29:33 +0330

    add new API call in ProductRepository.js

--- repositories/ProductRepository.js
+++ repositories/ProductRepository.js
@@ -12,10 +12,15 @@ export default ($axios) => ({
   getTypesByCategoryId(categoryId) {
     return $axios.get(`${resource}/category/${categoryId}/type`)
   },
-  getSimilarProductsByProductId(productId) {
-    return $axios.get(`${resource}/similar/${productId}`)
+  getSimilarProductsByProductId(productId,size,from) {
+    return $axios.get(`${resource}/similar/${productId}?size=${size}&from=${from}`)
   },
-
+  getOffProducts(size,from) {
+    return $axios.get(`${resource}/off-products?size=${size}&from=${from}`)
+  },
+  getNewProducts(size,from) {
+    return $axios.get(`${resource}/new-products?size=${size}&from=${from}`)
+  },
   getProductById(productId) {
     return $axios.get(`${resource}/${productId}`)
   },
