ec0d7342b6b1b089f5da5c8b0d8575993e681bf8
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-02 18:47:19 +0330

    first step of search page

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,13 +1,63 @@
 <template>
-  <H1>Search</H1>
+  <div>
+    <div class="search-header">
+      <v-row no-gutters>
+        <v-col>
+          <v-text-field
+            class="text-field"
+            placeholder="جستجو کنید"
+            hide-details
+            single-line
+            filled
+            dense
+            rounded>
+          </v-text-field>
+        </v-col>
+        <v-col class="search-btn">
+          <v-btn icon>
+            <v-icon color="primary">mdi-magnify</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </div>
+    <!--    <VerticalProductList :products="" />-->
+  </div>
 </template>
 
 <script>
+import VerticalProductList from '@/components/products/vertical/VerticalProductList'
+
 export default {
-name: "index.vue"
+  name: 'index.vue',
+  components: { VerticalProductList }
 }
 </script>
 
 <style scoped>
+.search-header {
+  position: fixed;
+  position: -webkit-sticky;
+  top: 0;
+  display: flex;
+  width: 100%;
+  z-index: 7;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  background-color: white;
+  direction: rtl;
+}
 
+.text-field {
+  width: 80vw;
+  max-width: 89vw;
+  padding: 0;
+  margin: 1.5vh;
+
+}
+
+.search-btn {
+  margin: auto;
+  text-align: center;
+}
 </style>
