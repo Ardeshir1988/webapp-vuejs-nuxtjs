5790f9e75ca363ef321c6619bc91971993aed876
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-02 16:24:34 +0330

    change Header.vue components for products page

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="header-container">
-    <div class="header-btn-back" @click="back">
-      <v-btn
-        class="btn-back"
-        icon>
-        <v-icon>mdi-arrow-left</v-icon>
-      </v-btn>
-    </div>
     <div class="header-name-page">
       {{ title }}
     </div>
@@ -30,34 +23,13 @@ export default {
   display: grid;
   grid-template-rows: 0 2fr;
   grid-template-columns: 1fr;
-  margin: 0.5vh;
-  height: 30px;
+  height: 6vh;
 }
 
 .header-name-page {
-  margin: 0.5vh;
   grid-column: 1;
   grid-row: 2;
   text-align: center;
   font-size: 1em;
-}
-
-.header-btn-back {
-  grid-column: 1;
-  grid-row: 1;
-  z-index: 1;
-  max-lines: 1;
-  text-align: left;
-  display: -webkit-box;
-  -webkit-line-clamp: 1;
-  -webkit-box-orient: vertical;
-  vertical-align: center;
-}
-
-.btn-back {
-  background-color: white;
-  height: 24px;
-  width: 24px;
-  margin: 0.5vh 0.5vh 1vh;
 }
 </style>
