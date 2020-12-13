5936ef9960ac0f442e7644e39306c9ccb0434c1f
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-13 21:52:22 +0330

    impl account page

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,13 +1,63 @@
 <template>
-<H1>Account</H1>
+  <div>
+    <Header class="header" title="حساب کاربری" />
+    <Register :login="login" v-if="register" class="register-container" />
+    <RegisterMobile v-if="registerMobile" @regmobile="sendOtpMobile" />
+    <VerifyOtp v-if="otp" @sendotp="verifyOtp" />
+    <RegisteredCustomer v-if="customer" />
+    <AccountGridOptions class="account-grid-options" />
+  </div>
 </template>
 
 <script>
+import AccountGridOptions from '@/components/acount/AccountGridOptions'
+import Register from '@/components/acount/Register'
+import Header from '@/components/header/Header'
+import RegisterMobile from '@/components/acount/RegisterMobile'
+import VerifyOtp from '@/components/acount/VerifyOtp'
+import RegisteredCustomer from '@/components/acount/RegisteredCustomer'
+
 export default {
-name: "index.vue"
+  name: 'index.vue',
+  components: { RegisteredCustomer, VerifyOtp, RegisterMobile, Header, Register, AccountGridOptions },
+  data() {
+    return {
+      register: true,
+      registerMobile: false,
+      otp: false,
+      customer: false
+    }
+  },
+  methods: {
+    login() {
+      this.register = false
+      this.registerMobile = true
+    },
+    sendOtpMobile(mobile) {
+      this.registerMobile = false
+      this.otp = true
+      console.log(mobile)
+    },
+    verifyOtp(otp) {
+      this.otp = false
+      this.customer = true
+      console.log(otp)
+    }
+  }
 }
 </script>
 
 <style scoped>
+.header {
+  width: 100%;
+}
 
+.account-grid-options {
+  bottom: 10vh;
+  position: fixed;
+}
+
+.register-container {
+  margin-top: 2vh;
+}
 </style>
