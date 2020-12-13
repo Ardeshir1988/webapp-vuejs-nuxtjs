b3f6ce484148d2757cfaa2ca326db015471f0d2d
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-13 21:47:34 +0330

    Impl AccountGridOptions.vue & AccountOption.vue components

---
+++ components/acount/AccountGridOptions.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="option-list">
+    <div v-for="op in options">
+    <AccountOption :op="op"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import AccountOption from '@/components/acount/AccountOption'
+export default {
+  name: 'AccountGridOptions',
+  components: { AccountOption },
+  data() {
+    return {
+      options: [
+        { name: 'وضعیت سفارش', iconName: 'order-status-24px.svg' },
+        { name: 'خریدهای گذشته', iconName: 'orders-24px.svg' },
+        { name: 'پشتیبانی', iconName: 'call-24px.svg' },
+        { name: 'آدرس ها', iconName: 'addresses-24px.svg' },
+        { name: 'معرفی به دوستان', iconName: 'share-24px.svg' },
+        { name: 'ارسال پیام', iconName: 'send-message-24px.svg' },
+        { name: 'قوانین', iconName: 'rules-24px.svg' },
+        { name: 'راهنما', iconName: 'info-24px.svg' },
+        { name: 'خروج', iconName: 'logout-24px.svg' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.option-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ components/acount/AccountOption.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-sheet class="account-option" outlined rounded>
+    <v-img class="img-option" :src="op.iconName"></v-img>
+    <div class="name-option">{{op.name}}</div>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'AccountOption',
+  props: { op: Object }
+}
+</script>
+
+<style scoped>
+.img-option {
+  width: 24px;
+  height: 24px;
+  margin: 3.5vh auto auto;
+}
+
+.name-option {
+  font-size: 10pt;
+  align-items: center;
+  margin-top: 2vh;
+}
+
+.account-option {
+  text-align: center;
+  height: 14vh;
+  width: 30vw;
+  margin: 1.6vw;
+}
+</style>
