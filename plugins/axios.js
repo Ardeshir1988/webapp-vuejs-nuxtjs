d94cdd8d418de744c12b26388f68e014be8e4bea
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-15 00:02:43 +0330

    handle axios exceptions

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,9 +3,14 @@ export default function({ $axios, redirect, $notifier }) {
     console.log('Making request to ' + config.url)
   })
   $axios.onError(error => {
-    if (error.response === undefined)
+    if (error.response === undefined) {
       $notifier.showMessage({ content: 'خطا در برقراری ارتباط با سرور', color: 'black' })
-    else
-      $notifier.showMessage({ content: error.response.data.message, color: 'black' })
+      return Promise.resolve(false);
+    } else {
+      if (error.response.data.message !== undefined)
+        $notifier.showMessage({ content: error.response.data.message, color: 'black' })
+      else
+        $notifier.showMessage({ content: 'خطا در برقراری ارتباط با سرور', color: 'black' })
+    }
   })
 }
