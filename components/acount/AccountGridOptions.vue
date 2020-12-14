e6797e380927901f1c13335ec2fcb10e4d9ff846
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-15 00:01:12 +0330

    add some additional properties in AccountGridOptions.vue

--- components/acount/AccountGridOptions.vue
+++ components/acount/AccountGridOptions.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="option-list">
     <div v-for="op in options">
-    <AccountOption :op="op"/>
+      <AccountOption :op="op" :key="op.id" />
     </div>
   </div>
 </template>
 
 <script>
 import AccountOption from '@/components/acount/AccountOption'
+
 export default {
   name: 'AccountGridOptions',
   components: { AccountOption },
   data() {
     return {
       options: [
-        { name: 'وضعیت سفارش', iconName: 'order-status-24px.svg' },
-        { name: 'خریدهای گذشته', iconName: 'orders-24px.svg' },
-        { name: 'پشتیبانی', iconName: 'call-24px.svg' },
-        { name: 'آدرس ها', iconName: 'addresses-24px.svg' },
-        { name: 'معرفی به دوستان', iconName: 'share-24px.svg' },
-        { name: 'ارسال پیام', iconName: 'send-message-24px.svg' },
-        { name: 'قوانین', iconName: 'rules-24px.svg' },
-        { name: 'راهنما', iconName: 'info-24px.svg' },
-        { name: 'خروج', iconName: 'logout-24px.svg' }
+        { id: 'op1', name: 'وضعیت سفارش', iconName: 'order-status-24px.svg', disable: true },
+        { id: 'op2', name: 'خریدهای گذشته', iconName: 'orders-24px.svg', disable: true },
+        { id: 'op3', name: 'آدرس ها', iconName: 'addresses-24px.svg', disable: true },
+        { id: 'op4', name: 'پشتیبانی', iconName: 'call-24px.svg', disable: false },
+        { id: 'op5', name: 'معرفی به دوستان', iconName: 'share-24px.svg', disable: false },
+        { id: 'op6', name: 'ارسال پیام', iconName: 'send-message-24px.svg', disable: true },
+        { id: 'op7', name: 'قوانین', iconName: 'rules-24px.svg', disable: false },
+        { id: 'op8', name: 'راهنما', iconName: 'info-24px.svg', disable: false },
+        { id: 'op9', name: 'خروج', iconName: 'logout-24px.svg', disable: true }
       ]
     }
   }
