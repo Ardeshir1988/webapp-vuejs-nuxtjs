f449a27b2e7307c4bc7bde4c4e2475c97da58338
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-19 12:25:06 +0330

    impl RegisterRepository.js

--- repositories/RegisterRepository.js
+++ repositories/RegisterRepository.js
@@ -3,5 +3,8 @@ const resource = '/register'
 export default ($axios) => ({
   registerMobile(mobileDto) {
       return $axios.post(`${resource}`, mobileDto)
+  },
+  sendOtp(otp){
+    return $axios.put(`${resource}`, otp)
   }
 })
