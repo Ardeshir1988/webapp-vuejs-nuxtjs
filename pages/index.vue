a79ea00d2bbccd509bb239d942884abace8a8645
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-19 12:26:34 +0330

    add refresh util component in home

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,34 @@
 <template>
   <div>
-    <Banner :banners="homepage.banners" />
-    <SectionSeparator class="section-separator"
-                      section-name="محصولات جدید"
-                      section-icon="storefront-24px.svg"
-                      section-link-url="/products/new" />
-    <HorizontalProductList :products="homepage.newProducts" />
-    <SectionSeparator class="section-separator"
-                      section-name="دسته بندی محصولات"
-                      section-icon="category-24px.svg"
-                      section-link-url="" />
-    <CategoryList :categories="homepage.categories" />
-    <Banner class="section-separator" :banners="homepage.banners" />
+    <RefreshUtil v-if="reload" />
+    <div v-if="!reload">
+      <Banner :banners="homepage.banners" />
+      <SectionSeparator class="section-separator"
+                        section-name="محصولات جدید"
+                        section-icon="storefront-24px.svg"
+                        section-link-url="/products/new" />
+      <HorizontalProductList :products="homepage.newProducts" />
+      <SectionSeparator class="section-separator"
+                        section-name="دسته بندی محصولات"
+                        section-icon="category-24px.svg"
+                        section-link-url="" />
+      <CategoryList :categories="homepage.categories" />
+      <Banner class="section-separator" :banners="homepage.banners" />
 
-    <SectionSeparator class="section-separator"
-                      section-name="شرکای تجاری"
-                      section-icon="partner-24px.svg"
-                      section-link-url="" />
+      <SectionSeparator class="section-separator"
+                        section-name="شرکای تجاری"
+                        section-icon="partner-24px.svg"
+                        section-link-url="" />
 
-    <BusinessPartnerList :business-partners="homepage.businessPartners" />
-    <SectionSeparator class="section-separator"
-                      section-name="حراج"
-                      section-icon="storefront-24px.svg"
-                      section-link-url="/products/off" />
-    <HorizontalProductList :products="homepage.offProducts" />
-    <div class="null-div"></div>
+      <BusinessPartnerList :business-partners="homepage.businessPartners" />
+      <SectionSeparator class="section-separator"
+                        section-name="حراج"
+                        section-icon="storefront-24px.svg"
+                        section-link-url="/products/off" />
+      <HorizontalProductList :products="homepage.offProducts" />
+      <div class="null-div"></div>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -34,14 +36,15 @@ import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import Banner from '@/components/banner/Banner'
 import SectionSeparator from '@/components/separator/SectionSeparator'
-import { mapState } from 'vuex'
 import HorizontalProductItem from '@/components/products/horizontal/HorizontalListProductItem'
 import HorizontalProductList from '@/components/products/horizontal/HorizontalProductList'
 import CategoryList from '@/components/category/CategoryList'
 import BusinessPartnerList from '@/components/partner/BusinessPartnerList'
+import RefreshUtil from '@/components/utils/RefreshUtil'
 
 export default {
   components: {
+    RefreshUtil,
     BusinessPartnerList,
     CategoryList,
     HorizontalProductList,
@@ -53,19 +56,17 @@ export default {
   },
   data() {
     return {
-      homepage: {}
+      homepage: {},
+      reload: false
     }
   },
-  async asyncData ({$repositories}) {
+  async asyncData({ $repositories }) {
     let responseData = await $repositories.product.homepage()
-    if (responseData.status !== 200)
-      this.showSnackbar()
-    return { homepage: responseData.data }
-  },
-  methods:{
-    showSnackbar (msg) {
-      this.$notifier.showMessage({ content: msg, color: 'black' })
-    }
+    console.log(responseData)
+    if (responseData === false)
+      return { reload: true }
+    else
+      return { homepage: responseData.data }
   }
 }
 </script>
@@ -74,7 +75,8 @@ export default {
   margin-top: 1vh;
   margin-bottom: 1vh;
 }
-.null-div{
+
+.null-div {
   height: 1vh;
 }
 </style>
