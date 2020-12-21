0549bddd82136a9d7c63d629ced16cebf41c4a5a
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-21 14:46:46 +0330

    add update_cart & delete_product & update_product_stock

--- store/cart.js
+++ store/cart.js
@@ -9,11 +9,17 @@ export const mutations = {
 
 export const actions = {
   async increase_product({ commit }, product) {
+    let pic = ''
+    if (product.quantity === undefined) {
+      pic = product.pictures[0]
+    } else {
+      pic = product.pictures
+    }
     const cartProduct = {
       id: product.id,
       name: product.name,
       measure: product.measure,
-      picture:product.pictures,
+      picture: pic,
       price: product.price,
       discountPrice: product.discountPrice,
       stepDiscountPrice: product.stepDiscountPrice,
@@ -38,7 +44,6 @@ export const actions = {
       }
       await this.$localForage.setItem('cart', cart)
     }
-
     commit('UPDATE_CART', cart)
   },
   async decrease_product({ commit }, product) {
@@ -51,6 +56,21 @@ export const actions = {
     await this.$localForage.setItem('cart', cart)
     commit('UPDATE_CART', cart)
   },
+  async delete_product({ commit }, productId) {
+    let cart = await this.$localForage.getItem('cart')
+    cart = cart.filter(p => p.id !== productId)
+    await this.$localForage.setItem('cart', cart)
+    commit('UPDATE_CART', cart)
+  },
+  async update_product_stock({ commit }, product) {
+    let cart = await this.$localForage.getItem('cart')
+    const cartProductIndex = cart.map(p => p.id).indexOf(product.id)
+    cart[cartProductIndex].stock = product.stock
+    await this.$localForage.setItem('cart', cart)
+    console.log('update cart')
+    console.log(cart)
+    commit('UPDATE_CART', cart)
+  },
   async init_cart({ commit }) {
     let cart = await this.$localForage.getItem('cart')
     if (cart == null) {
@@ -59,6 +79,22 @@ export const actions = {
       await this.$localForage.setItem('cart', initCart)
     } else
       commit('UPDATE_CART', cart)
+  },
+  async update_cart({ commit }) {
+    let cartList = await this.$localForage.getItem('cart')
+    const responseData = await this.$repositories.product.checkCartProductsAvailability({ 'products': cartList })
+    if (responseData !== false) {
+      responseData.data.products.forEach(serverProduct => {
+        cartList.forEach(cartProduct => {
+          if (cartProduct.stock !== serverProduct.stock) {
+            const cartProductIndex = cartList.map(p => p.id).indexOf(serverProduct.id)
+            cartList[cartProductIndex].stock = serverProduct.stock
+            this.$localForage.setItem('cart', cartList)
+            commit('UPDATE_CART', cartList)
+          }
+        })
+      })
+    }
   }
 }
 
