3a6462ac571bf8a9298f6a9d42ae3b2be0641e40
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-21 17:19:46 +0330

    impl category page

--- pages/category/index.vue
+++ pages/category/index.vue
@@ -1,13 +1,30 @@
 <template>
-
+  <CategoryList class="category" :categories="categories" />
 </template>
 
 <script>
+import CategoryList from '@/components/category/CategoryList'
+
 export default {
-name: "index"
+  name: 'index',
+  components: { CategoryList },
+  data() {
+    return {
+      categories: []
+    }
+  },
+  async fetch() {
+    const categoryList = await this.$repositories.product.getCategories()
+    if (categoryList !== false) {
+      this.categories = categoryList.data
+    } else {
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.category {
+  margin-top: 1vh;
+}
 </style>
