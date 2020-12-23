1317177e106b2c57d5f09f70f4c2eaddb4517bd8
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-23 12:34:12 +0330

    impl AddressList.vue component

---
+++ components/address/AddressList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <AddressItem v-for="address in addresses" :address="address" :key="address.id" />
+  </div>
+</template>
+
+<script>
+import AddressItem from '@/components/address/AddressItem'
+
+export default {
+  name: 'AddressList',
+  components: { AddressItem },
+  props: { addresses: Array }
+}
+</script>
+
+<style scoped>
+
+</style>
