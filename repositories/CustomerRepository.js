73600f6556b1fca4017e4a0146e38a0b1567a827
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-23 12:43:52 +0330

    add new APIs for customer address

--- repositories/CustomerRepository.js
+++ repositories/CustomerRepository.js
@@ -9,5 +9,23 @@ export default ($axios) => ({
   },
   updateProfile(customerProfileDto) {
     return $axios.put(`${resource}/profile`, customerProfileDto)
+  },
+  getAddresses() {
+    return $axios.get(`${resource}/address`)
+  },
+  getAddressById(addressId) {
+    return $axios.get(`${resource}/address/${addressId}`)
+  },
+  updateAddress(address) {
+    return $axios.put(`${resource}/address`, address)
+  },
+  saveAddress(address) {
+    return $axios.post(`${resource}/address`, address)
+  },
+  changeSelectedAddress(addressId) {
+    return $axios.put(`${resource}/address/${addressId}/select`)
+  },
+  deleteSelectedAddress(addressId) {
+    return $axios.delete(`${resource}/address/${addressId}`)
   }
 })
