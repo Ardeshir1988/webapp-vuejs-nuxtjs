a80d2b407629ae735fa88e51a09cc6e802e89d88
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-23 12:33:27 +0330

    impl CustomerAddress.vue component

---
+++ components/address/CustomerAddress.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="height: 55vh">
+    <client-only>
+      <l-map @click="changeMarkerPosition" :zoom=15 :center="map">
+        <l-tile-layer :url="url"></l-tile-layer>
+        <l-marker :lat-lng="marker"></l-marker>
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CustomerAddress',
+  props: { center: Object, markerLoc: Object },
+  data() {
+    return {
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      map: this.center,
+      marker: this.markerLoc
+    }
+  },
+  methods: {
+    changeMarkerPosition(event) {
+      this.marker = event.latlng
+      this.$emit('changelocation', this.marker)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
