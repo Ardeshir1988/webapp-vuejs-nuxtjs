038d642747a3cddcda68314148d13674bdccd6df
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-26 20:09:09 +0330

    add api call

--- repositories/OrderRepository.js
+++ repositories/OrderRepository.js
@@ -3,5 +3,8 @@ const resource = '/order'
 export default ($axios) => ({
   checkoutOrder(orderDto) {
     return $axios.post(`${resource}`, orderDto)
+  },
+  getCurrentOrders() {
+    return $axios.get(`${resource}`)
   }
 })
