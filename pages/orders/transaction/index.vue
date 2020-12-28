ff3399971abddfd4d4efdad45820fd7b0c202c92
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-28 14:59:31 +0330

    impl first step of transaction page

---
+++ pages/orders/transaction/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Header class="fix-header" title="سفارش ها و تراکنش ها" />
+    <TransactionList :transactions="transactions" />
+  </div>
+</template>
+
+<script>
+import OrderTransaction from '@/components/transaction/OrderTransaction'
+import PaymentTransaction from '@/components/transaction/PaymentTransaction'
+import RefundTransaction from '@/components/transaction/RefundTransaction'
+import ReturnProductTransaction from '@/components/transaction/ReturnProductTransaction'
+import TransactionList from '@/components/transaction/TransactionList'
+
+export default {
+  name: 'index',
+  components: { TransactionList, ReturnProductTransaction, RefundTransaction, PaymentTransaction, OrderTransaction },
+  async asyncData({ $repositories }) {
+    const transactionList =await $repositories.order.getTransactions()
+    if (transactionList !== false) {
+      return {
+        transactions: transactionList.data
+      }
+    } else {
+      return {
+        reload: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
