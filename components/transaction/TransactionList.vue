c3090d1b3d3bf4a3f9766c10ce086aa055bc48d9
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-28 15:13:24 +0330

    impl TransactionList.vue component

---
+++ components/transaction/TransactionList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="item in transactions">
+      <OrderTransaction @click="getDetails" v-if="item.type === 'ORDER'" :transaction="item" :key="item.id" />
+      <ReturnProductTransaction @click="getDetails" v-if="item.type === 'PRODUCT'" :transaction="item" :key="item.id" />
+      <RefundTransaction v-if="item.type === 'REFUND'" :transaction="item" :key="item.id" />
+      <PaymentTransaction v-if="item.type === 'PAYMENT'" :transaction="item" :key="item.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+import OrderTransaction from '@/components/transaction/OrderTransaction'
+import ReturnProductTransaction from '@/components/transaction/ReturnProductTransaction'
+import RefundTransaction from '@/components/transaction/RefundTransaction'
+import PaymentTransaction from '@/components/transaction/PaymentTransaction'
+
+export default {
+  name: 'TransactionList',
+  components: { PaymentTransaction, RefundTransaction, ReturnProductTransaction, OrderTransaction },
+  props: { transactions: Array },
+  methods: {
+    async getDetails() {
+      this.$repositories.order.getCurrentOrderDetail(this.order.reference)
+        .then(res => {
+          if (res.data !== false) {
+            this.orderDetails = res.data
+            this.sheet = true
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
