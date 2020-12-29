1dace184647590a03248d2539686afb4913a6edb
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-12-29 12:02:41 +0330

    modify ssr-cookie.js middleware

--- middleware/ssr-cookie.js
+++ middleware/ssr-cookie.js
@@ -1,13 +1,3 @@
-export default function({ app, req, $axios }) {
-  if (process.server){
-    if(req.headers.cookie !==undefined)
-    $axios.defaults.headers.common = { 'Authorization': 'Bearer ' + req.headers.cookie.substr(6) }
-  }
-  else {
-    const c = app.$cookies.get('token')
-    console.log(c)
-    console.log('sssr')
-    if (c !== undefined)
-    $axios.setHeader('Authorization', 'Bearer ' + c)
-  }
+export default function({ app, $axios }) {
+    $axios.defaults.headers.common = { 'Authorization': 'Bearer ' +app.$cookies.get('token') }
 }
