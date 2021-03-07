a22b26679393db3c1584f644e42bd1bda827fd7a
ardeshir <ardeshir.ahouri@gmail.com>
2021-03-07 14:55:22 +0330

    change pwa icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,16 +83,9 @@ export default {
       start_url: "/",
       icons: [
         {
-          src: "/manifest-icon-192.png",
-          sizes: "192x192",
+          src: "/hyperjet-icon.png",
           type: "image/png",
-          purpose: "maskable any"
-        },
-        {
-          src: "/manifest-icon-512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable any"
+          purpose: "any"
         }
       ],
 
