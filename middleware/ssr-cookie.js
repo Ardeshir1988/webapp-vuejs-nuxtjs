69f59dc7c21147cd06c0be264b123945e262b3fa
max <m.asadi@gmail.com>
2021-04-06 14:06:58 +0430

    Search Without Login

--- middleware/ssr-cookie.js
+++ middleware/ssr-cookie.js
@@ -1,3 +1,4 @@
 export default function({ app, $axios }) {
+  if (app.$cookies.get('token') !== undefined)
     $axios.defaults.headers.common = { 'Authorization': 'Bearer ' +app.$cookies.get('token') }
 }
