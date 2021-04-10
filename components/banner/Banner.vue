4eb1a8dd1df9a15ea47e0734c7158539ecac5abe
max <m.asadi@gmail.com>
2021-04-10 09:37:53 +0430

    Top banner size modified

--- components/banner/Banner.vue
+++ components/banner/Banner.vue
@@ -1,19 +1,31 @@
 <template>
-  <v-carousel style="padding: 3vw" cycle
-              height="auto"
-              show-arrows-on-hover
-              hide-delimiters>
-
-    <v-carousel-item
-      v-for="(item,i) in bannersUrl"
-      :key="i"
-      :src="item"
-    ></v-carousel-item>
-  </v-carousel>
+  <v-img
+    :src='bannersUrl[0]'
+    style='margin: 3vw'
+  >
+    <v-card
+      elevation='0'
+      height='100%'
+    >
+      <v-carousel cycle
+                  show-arrows-on-hover
+                  hide-delimiters
+                  height='100%'
+      >
+        <v-carousel-item
+          v-for='(item,i) in bannersUrl'
+          :key='i'
+          :src='item'
+        >
+        </v-carousel-item>
+      </v-carousel>
+    </v-card>
+  </v-img>
 </template>
 
 <script>
 import { FILES_URL } from '~/constants'
+
 export default {
   name: 'Banner',
   props: { banners: Array },
