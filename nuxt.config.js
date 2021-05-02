2d23dd84e0a05952d78afa5b93048536d60a6eb6
max <m.asadi@gmail.com>
2021-05-02 17:03:16 +0430

    Nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'server',
-  mode: 'universal', // or 'spa'
+  // mode: 'universal', // or 'spa'
   // target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   // debug: true,
