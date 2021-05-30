2d421e5d92da6069db9017974455a22aa47d3bc9
max <m.asadi@gmail.com>
2021-05-30 08:30:06 +0430

    Index loading

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <loading-animation v-if="!ready"/>
     <RefreshUtil v-if="reload" />
     <div v-if="!reload && ready">
-      <Banner v-if="instructions.topBannerPermission" :banners="homepage.topBanners"/>
+      <Banner v-if="instructions.topBannerPermission" :banners="topBanners"/>
       <SectionSeparator class="section-separator"
                         section-name="محصولات جدید"
                         :section-icon="require('~/assets/icon/storefront-24px.svg')"
@@ -15,7 +15,7 @@
                         :section-icon="require('~/assets/icon/category-24px.svg')"
                         section-link-url="/category" />
       <CategoryList :categories="homepage.categories" />
-      <Banner v-if="instructions.bottomBannerPermission" class="section-separator" :banners="homepage.bottomBanners" />
+      <Banner v-if="instructions.bottomBannerPermission" class="section-separator" :banners="bottomBanners" />
 
       <SectionSeparator v-if="instructions.businessPartnerPermission" class="section-separator"
                         section-name="شرکای تجاری"
@@ -42,7 +42,7 @@ import CategoryList from '@/components/category/CategoryList'
 import BusinessPartnerList from '@/components/partner/BusinessPartnerList'
 import RefreshUtil from '@/components/utils/RefreshUtil'
 import LoadingAnimation from '@/components/utils/LoadingAnimation'
-
+import { mapGetters } from 'vuex'
 export default {
   head:{
     title:'هایپرجت ، هوشمندانه ترین راه خرید نیاز های روزانه شما'
@@ -59,31 +59,74 @@ export default {
   },
   data() {
     return {
-      homepage: {},
-      instructions: {},
-      reload: false,
-      ready: false
+      // homepage: {},
+      // instructions: {},
     }
+  },
+  computed: {
+    // mix the getters into computed with object spread operator
+    ...mapGetters({
+      instructions: 'instruction/getSysInstruction'
+    }) ,
+    ...mapGetters({
+      homepage: 'instruction/getHomeInstruction'
+    }),
+    ...mapGetters({
+      topBanners: 'instruction/getTopBanners'
+    }),
+    ...mapGetters({
+      bottomBanners: 'instruction/getBottomBanners'
+    })
+,
+    ...mapGetters({
+      ready: 'instruction/getHomeStateReady'
+    })
+,
+    ...mapGetters({
+      reload: 'instruction/getHomeStateReload'
+    })
+
   },
   async beforeCreate() {
-    console.log('beforeCreate index')
-    let responseData =  await this.$repositories.product.homepage()
-    let resInstruction = await this.$repositories.product.getInstructions()
-    if (responseData === false || resInstruction === false){
-       this.reload = true
-      this.ready = true
-    }
-    else {
-      this.homepage = responseData.data
-      this.instructions = resInstruction.data
-      await this.$store.dispatch('instruction/load_home_instruction', responseData.data)
-      await this.$store.dispatch('instruction/load_sys_instruction', resInstruction.data)
-      this.ready = true
-    }
+    // this.instructions = await this.$store.getters['instruction/getSysInstruction']
+    // // console.log(this.$store.getters['instruction/getHomeInstruction'])
+    // // console.log(this.$store.getters['instruction/getSysInstruction'])
+    // if (this.homepage === undefined && this.instructions === undefined) {
+    //   let responseData = await this.$repositories.product.homepage()
+    //   let resInstruction = await this.$repositories.product.getInstructions()
+    //   if (responseData === false || resInstruction === false) {
+    //     this.reload = true
+    //     this.ready = true
+    //   } else {
+    //   this.homepage = responseData.data
+    //     this.instructions = resInstruction.data
+    //   await this.$store.dispatch('instruction/load_home_instruction', responseData.data)
+    //   await this.$store.dispatch('instruction/load_sys_instruction', resInstruction.data)
+    //   this.ready = true
+    // }
+    // }
+    // else{
+    // }
   },
 
-  async asyncData({ $repositories, app, store}) {
+  async asyncData({ $repositories,$storage, app, store})
+  {
+   // return{
+   //   homepage: store.getters['instruction/getHomeInstruction'],
+   //   instructions: store.getters['instruction/getSysInstruction']
+   // }
+      // .then(
 
+      // res =>
+      //   return { homepage: res }
+  // )
+
+
+      // .then(
+      // {
+      //   return { instructions: res}
+      // }
+    // )
   }
 }
 </script>
