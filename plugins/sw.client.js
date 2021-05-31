cb30e82a172497ef2c618252e6f52c03ec38f4c6
max <m.asadi@gmail.com>
2021-05-31 12:29:07 +0430

    service worker

--- plugins/sw.client.js
+++ plugins/sw.client.js
@@ -12,20 +12,15 @@
   //   })
   // }
 
-  // const workbox =  window.$workbox;
-  // if (workbox) {
-  //
-  //   workbox.addEventListener('installed', (event) => {
-  //     // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
-  //     if (event.isUpdate) {
-  //       // this.$notifier.showMessage({ content: 'اپلیکشن شما به روز رسانی شد.', color: 'success', title: 'بروز رسانی' , icon: 'mdi-check-circle-outline' })
-  //       // whatever logic you want to use to notify the user that they need to refresh the page.
-  //       caches.keys().then(function(names) {
-  //         for (let name of names) caches.delete(name)
-  //       })
-  //       window.location.reload(true)
-  //     }
-  //   })
-  // }
+  const workbox =  window.$workbox;
+  if (workbox) {
+
+    workbox.addEventListener('installed', (event) => {
+      // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
+      if (event.isUpdate) {
+        window.location.reload(true)
+      }
+    })
+  }
 
 // }
