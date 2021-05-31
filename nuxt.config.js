892fc09eaaf270d498e6a7c7cff572d3e2dbbb5b
max <m.asadi@gmail.com>
2021-05-31 15:46:08 +0430

    Nuxt Config

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,19 +14,16 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-
+    ]
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/styles/main.css'
+  css: ['~/assets/styles/main.css', 'material-design-icons-iconfont/dist/material-design-icons.css'
   ],
   layoutTransition: {
     name: 'layout',
     mode: 'out-in'
   },
+
   loading: {
     color: '#8629FD',
     height: '4px'
@@ -91,48 +88,17 @@ export default {
       lang: 'en',
       description: 'Hyper Jet online shopping',
       background_color: '#FFFFFF',
-      ogImage: 'hyperjet-icon.png',
       start_url: '/',
-      icons: [
-        {
-          src: '/icons/ic_hjet_64.png',
-          sizes: '64x64',
-          type: 'image/png'
-        },
-        {
-          src: '/icons/ic_hjet_120.png',
-          sizes: '120x120',
-          type: 'image/png'
-        },
-        {
-          src: '/icons/ic_hjet_144.png',
-          sizes: '144x144',
-          type: 'image/png'
-        },
-        {
-          src: '/icons/ic_hjet_152.png',
-          sizes: '152x152',
-          type: 'image/png'
-        },
-        {
-          src: '/icons/ic_hjet_192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/icons/ic_hjet_384.png',
-          sizes: '384x384',
-          type: 'image/png'
-        },
-        {
-          src: '/icons/ic_hjet_512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ],
       display: 'fullscreen',
       orientation: 'portrait'
     },
+    icon: {
+      source: "~/static/icons/icon.png",
+      purpose: 'maskable'
+    },
+    meta: [
+      { name: 'apple-mobile-web-app-capable', content: 'yes' }
+    ],
     workbox: {
       dev: true,
       enabled: true,
@@ -172,118 +138,10 @@ export default {
       }],
       cacheAssets: true,
     }
+
   },
-  meta: [
-    { name: 'apple-mobile-web-app-capable', content: 'yes' }
-  ],
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' },
-    // apple
 
-    { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-icon-180.png' },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2048-2732.jpg',
-      media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2732-2048.jpg',
-      media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1668-2388.jpg',
-      media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1536-2048.jpg',
-      media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2048-1536.jpg',
-      media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1668-2224.jpg',
-      media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2224-1668.jpg',
-      media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1620-2160.jpg',
-      media: '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2160-1620.jpg',
-      media: '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1284-2778.jpg',
-      media: '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2778-1284.jpg',
-      media: '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1170-2532.jpg',
-      media: '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2532-1170.jpg',
-      media: '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1125-2436.jpg',
-      media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2436-1125.jpg',
-      media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2688-1242.jpg',
-      media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1242-2688.jpg',
-      media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-828-1792.jpg',
-      media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1792-828.jpg',
-      media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1242-2208.jpg',
-      media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-2208-1242.jpg',
-      media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-    },
 
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-750-1334.jpg',
-      media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1334-750.jpg',
-      media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-    },
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-640-1136.jpg',
-      media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-    },
-
-    {
-      rel: 'apple-touch-startup-image', href: '/static/apple-splash-1136-640.jpg',
-      media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-    }
-  ],
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     plugins: [
